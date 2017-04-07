239e06a1a1ad7f4c7acdf5735d918901dbfc941c
Aviral Garg <vrlgarg@gmail.com>
2017-04-07 21:55:47 +0530

    all routes ready

--- public/javascripts/listing_angular.js
+++ public/javascripts/listing_angular.js
@@ -1,6 +1,7 @@
 var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope, $http) {
   $http.get("/api/get-all-property").then(function (response) {
+  	console.log("Loading..");
       $scope.myData = response.data.data;
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,29 @@ router.get('/', function(req, res) {
 });
 
 router.get('/submit-property', function(req, res) {
-  res.render('submit-property');
+  res.render('submit-property', { title:'Submit your Property and earn from it'});
+});
+
+router.get('/login-register', function(req, res) {
+  res.render('login-register', { title:'Signup or Register today and get best prices of your Property'});
+});
+
+router.get('/listings-list', function(req, res) {
+  res.render('listings-list', { title:'Find the best places to advertise in adsbowl'});
+});
+router.get('/my-profile', function(req, res) {
+  res.render('my-profile', { title:'Connect with numerous of advertisers in adsbowl'});
+});
+router.get('/change-password', function(req, res) {
+  res.render('change-password', { title:'Sercure your password-Adsbowl'});
+});
+router.get('/my-properties', function(req, res) {
+  res.render('my-properties', { title:'Find the best rates of property where you can advertise'});
+});
+router.get('/agents-list', function(req, res) {
+  res.render('agent-list', { title:'Agents on adsbowl'});
+});
+router.get('/agents-page', function(req, res) {
+  res.render('agent-page', { title:'Check out the profile of agents who can rent out your property on advertisement'});
 });
 module.exports = router;
